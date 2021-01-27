23a43ca840b489e770483125328ad34c1f9b9ba3
peoplesing1832 <peoplesing1832@gmail.com>
2021-01-28 07:58:31 +0800

    update

--- react/server/render.js
+++ react/server/render.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import { renderToString } from "react-dom/server";
+import { renderToString } from 'react-dom/server';
+import Home from '../client/views/Home';
 
 const render = () => {
+  const content = renderToString(<Home />)
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root">${content}</div>
+      </body>
+    </html>
+  `;
 };
 
 export default render;
