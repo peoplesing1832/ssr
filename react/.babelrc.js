ba83fcf0a088538c309ea60e99eb760a93c562e5
peoplesing1832 <peoplesing1832@gmail.com>
2021-01-28 08:05:48 +0800

    update

---
+++ react/.babelrc.js
@@ -0,0 +1,24 @@
+module.exports = function (api) {
+  const { NODE_ENV } = process.env
+  const isDevelopment = NODE_ENV === 'development'
+
+  if (api) {
+    api.cache.invalidate(() => NODE_ENV)
+  }
+
+  return {
+    presets: [
+      "@babel/preset-env",
+      {
+        "useBuiltIns": "entry",
+        "corejs": 3,
+      }
+      [
+        "@babel/preset-react",
+        {
+          development: isDevelopment
+        }
+      ]
+    ],
+  }
+}
