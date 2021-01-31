e6ef9bc69b652470d66588903fe49341a78052c8
peoplesing1832 <peoplesing1832@gmail.com>
2021-01-31 16:10:39 +0800

    update

--- react/client/hydrate.js
+++ react/client/hydrate.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import createStore from 'client/stroe';
 import App from 'client/App';
 
 ReactDOM.hydrate(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={createStore()}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
