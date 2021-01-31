3406743905543af8a600cac2b09ed792e9d209e6
peoplesing1832 <peoplesing1832@gmail.com>
2021-01-31 15:22:19 +0800

    update

--- react/client/store/index.js
+++ react/client/store/index.js
@@ -3,3 +3,18 @@ import {
   applyMiddleware,
   combineReducers
 } from 'redux';
+import thunk from 'redux-thunk';
+import {
+  homeReducer,
+  moreReducer,
+} from 'client/store/reducers';
+
+const reducer = combineReducers({
+  home: homeReducer,
+  more: moreReducer,
+});
+
+const store = () => {
+};
+
+export default store;
